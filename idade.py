87c461adbaba91d11a7c5548ad93ed3c53009f6c
Angélica <a.r.g.eng.soft@gmail.com>
2023-08-01 16:52:12 -0300

    idade

---
+++ idade.py
@@ -0,0 +1,12 @@
+
+print("="*17)
+print("Calcule sua Idade")
+print("="*17)
+
+  
+idade = 0
+nome = input("Nome Completo: ")
+ano = int(input("Nasceu em que ano? "))
+anoatual = 2022
+idade = anoatual - ano
+print( "Olá",nome, "sua idade é:", idade,)
