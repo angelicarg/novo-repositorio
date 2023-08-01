188ac81de236fb62a471ea35269504b2407c5f4f
Angélica <a.r.g.eng.soft@gmail.com>
2023-08-01 09:48:20 -0300

    atividade de andares

--- novo.py
+++ novo.py
@@ -1,1 +1,11 @@
 print("Hello Word")
+
+andar = 0
+def andares (andar):
+  for i in range(20):
+    andar = andar + 1
+    if (andar == 13):
+      continue
+    if (andar == 20):
+      break
+    print(andar)
